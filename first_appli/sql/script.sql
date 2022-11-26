a41075401da8dbcd7bdcf35290f2bc492c8e309d
hadil <hadilissilame@gmail.com>
2022-11-27 00:15:19 +0100

    update

--- first_appli/sql/script.sql
+++ first_appli/sql/script.sql
@@ -18,25 +18,38 @@ CREATE TABLE Client (
     ville VARCHAR(75) NOT NULL,
     telephone CHAR(10) NOT NULL,
     id_produit INT
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE Produit (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(50) NOT NULL,
-    image TEXT NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    prix FLOAT UNSIGNED NOT NULL,
+    reference VARCHAR(20) NOT NULL,
+    categorie VARCHAR(20) NOT NULL,
+    titre VARCHAR(100) NOT NULL,   
+    description TEXT NOT NULL,
     couleur VARCHAR(25) NOT NULL,
-    id_commande INT
-);
+    taille INT NOT NULL,
+    public ENUM('m','f','mixte') NOT NULL,
+    image TEXT NOT NULL,
+    prix FLOAT UNSIGNED NOT NULL,
+    stock FLOAT UNSIGNED NOT NULL,    
+    UNIQUE (reference)
+) ENGINE=InnoDB;
 
 CREATE TABLE Commande (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    date_achat DATE,
-    heure_achat TIMESTAMP,
-    id_produit INT,
-    id_client INT
-);
+    id_client INT NULL DEFAULT NULL,
+    montant INT NOT NULL,
+    date_enregistrement DATETIME NOT NULL,
+    etat ENUM('en cours de traitement', 'envoyé', 'livré') NOT NULL,    
+) ENGINE=InnoDB;
+
+CREATE TABLE detail_commande (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_commande INT NULL DEFAULT NULL,
+    id_produit INT NULL DEFAULT NULL,
+    quantite INT NOT NULL,
+    prix INT NOT NULL
+) ENGINE=InnoDB;
 
 --  ------------------------
 --  INSERTION DES DONNEES
@@ -52,3 +65,10 @@ INSERT INTO client VALUES
 (NULL,  "DELAVEINE" ,   "Bastien"   ,   "delaveine.bastien@example.com" ,  "Versailles" ,    "0671421282",   4),
 (NULL,  "DELAVEINE" ,   "Claire"    ,   "delaveine.claire@example.com"  ,  "Versailles" ,    "0781624326",   4),
 (NULL,  "ABDALLAH"  ,   "Myriam"    ,   "abdallah.myriam@example.com"   ,  "Paris"      ,    "0691827360",   5);
+
+INSERT INTO produit VALUES
+(NULL,  'DZ5485-303',   'chaussures',   'Air Jordan 1 Retro High OG Celtics',   'Chaussures Air Jordan 1 pour homme aux couleurs des Celtics de Boston' ,   'Green' ,   40  ,   'm' ,   'air-jordan-1-retro-high-og-gorge-green-dz5485-303.jpg' , 180.00,   1),
+(),                 
+(),
+(),
+();
